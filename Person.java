ef174dbb61835adeb164d9365c0ea8af1f349aac
Boz514 <boztunaozgur@gmail.com>
2022-10-08 16:15:22 -0700

    first update

--- Person.java
+++ Person.java
@@ -1,11 +1,11 @@
 public class Person {
   public String firstname;
   public String lastname;
+  public  int age;
 
-  public Person(String fname, String lname){
+  public Person(String fname, String lname, int age){
     this.firstname  = fname;
     this.lastname  = lname;
+    this.age=age;
   }
 
-  
-}
