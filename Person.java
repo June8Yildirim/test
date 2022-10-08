f42b17aeffcecb698f4fa19fa39e3792a3b5da77
cuneyt_yildirim <cuneyt.yildirim@dawsoncollege.qc.ca>
2022-10-08 18:50:47 -0400

    Initial commit

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person {
+  public String firstname;
+  public String lastname;
+
+  public Person(String fname, String lname){
+    this.firstname  = fname;
+    this.lastname  = lname;
+  }
+
+  
+}
---
+++ main.java
@@ -0,0 +1,5 @@
+public class{
+    public static void main(String[]args){
+        System.out.println("Hello this is main project for enterance");
+    }
+}
