73d4fb6b04f1810ee6a8f4a87c7be724ba953bc7
cuneyt_yildirim <cuneyt.yildirim@dawsoncollege.qc.ca>
2022-10-09 23:21:47 -0400

    erwer

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test{
     public static void main(String[]args){
         System.out.println("Hello this is main project for enterance");
+//        Student s1  = new Student("hasan","yildiz",12,"7a");
+//        System.out.println( s1.getGrade());
+        Person s2 = new Student("cuneyt","Yiliri",33,"12A");
+//        Employee e1 = new Employee("ozgur","yasar",34,"full-time");
+        Person e2 = new Employee("hasn","12",55,"part-type");
+
+        System.out.println( e2.getFirstname());
+        System.out.println(s2);
     }
 }
